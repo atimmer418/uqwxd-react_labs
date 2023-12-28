a3142359cb5b2cf8a3585f892f1c392e926e4006
Andrew Timmer <atimmer@terpmail.umd.edu>
2023-12-28 17:32:31 -0500

    actions, store, reducers in REDUX

--- react-redux-master/src/components/DivPanel.js
+++ react-redux-master/src/components/DivPanel.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import {useSelector} from 'react-redux'
 
+const DivPanel = () => {
+    let counterVal = useSelector(state => state.counter);
+    return (
+        <div>
+            {counterVal} This is how many times you've pressed the counter
+        </div>
+    );
+}
+
+export default DivPanel;
